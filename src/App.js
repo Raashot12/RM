4c4833982e358743e3b8e273fd9759e0978dc20f
Matthew Nte <Matthewnte1@gmail.com>
2020-10-17 03:39:26 +0100

    ft(create-event): add basic create event info [finishes]

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import React from 'react';
 // import Navbar from './Components/Navbar';
-import { BrowserRouter as Router, Switch, 
-	Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import './Grid.css'
-import Signup from './Components/pages/Signup&Login/Signup'
+import './Grid.css';
+import Signup from './Components/pages/Signup&Login/Signup';
 import Login from './Components/pages/Signup&Login/Login';
 import ResetPassword from './Components/pages/Signup&Login/ResetPassword';
 import Componentsfall from './Componentsfall';
 import Signupflow from './Components/pages/Signupflow/Signupflow';
-import Goal from "./Components/pages/Signupflow/Goal"
+import Goal from './Components/pages/Signupflow/Goal';
 // import NavbarFlow from "./Components/pages/Signupflow/Interest/NavbarFlowInterest"
-import RenderCheckbox from "./Components/pages/Signupflow/Interest/RenderCheckbox"
-import Topics from "./Components/pages/Signupflow/Interest/Topics"
-import Blog from "./Components/pages/Blog/Blog"
-import NavigationForNotLoggedin from "./Components/pages/Event&Page/NavigationForNotLoggedin"
-import createEvent from './pages/create-event/Create'
+import RenderCheckbox from './Components/pages/Signupflow/Interest/RenderCheckbox';
+import Topics from './Components/pages/Signupflow/Interest/Topics';
+import Blog from './Components/pages/Blog/Blog';
+import NavigationForNotLoggedin from './Components/pages/Event&Page/NavigationForNotLoggedin';
+import createEvent from './pages/create-event/Create';
 
 function App() {
   return (
-	<div>
-    <Router>
-			<Switch>
-				<Route path="/" component={Componentsfall} exact />
-				<Route path="/Login" component={Login} exact />
-				<Route path="/Signup" component={Signup} exact/>
-				<Route path="/ResetPassword" component={ResetPassword} exact />
-				<Route path="/Signupflow" component={Signupflow} exact />
-				<Route path="/Goal" component={Goal}  />
-				<Route path="/interestchecked" component={RenderCheckbox} exact />
-				<Route path="/topics" component={Topics} exact />
-				<Route path= "/blog" component={Blog} exact	/>
-				<Route path="/event" component={NavigationForNotLoggedin} exact />
-				<Route path="/create-event" component={createEvent} exact />
-			</Switch>
-		</Router>
-	  </div>
-
+    <div>
+      <Router>
+        <Switch>
+          <Route path='/' component={Componentsfall} exact />
+          <Route path='/Login' component={Login} exact />
+          <Route path='/Signup' component={Signup} exact />
+          <Route path='/ResetPassword' component={ResetPassword} exact />
+          <Route path='/Signupflow' component={Signupflow} exact />
+          <Route path='/Goal' component={Goal} />
+          <Route path='/interestchecked' component={RenderCheckbox} exact />
+          <Route path='/topics' component={Topics} exact />
+          <Route path='/blog' component={Blog} exact />
+          <Route path='/event' component={NavigationForNotLoggedin} exact />
+          <Route
+            path='/create-event/basic-info'
+            component={createEvent}
+            exact
+          />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- src/pages/create-event/components/BasicInfo.js
+++ src/pages/create-event/components/BasicInfo.js
@@ -2,7 +2,6 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
@@ -10,27 +9,28 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import '../styles/input.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    marginTop: 32,
+    margin: '32px 0 98px',
+    padding: '0 12px',
     color: 'rgba(0, 0, 0, .87)',
   },
   marginTop: {
     marginTop: theme.spacing(4),
+    [theme.breakpoints.down('xs')]: {
+      marginTop: theme.spacing(2),
+    },
   },
   inputTitle: {
     fontWeight: 'medium',
     marginBottom: theme.spacing(1),
   },
-  textField: {
-    width: '100%',
-  },
   header: {
     fontWeight: 'bold',
   },
   formControl: {
-    marginTop: theme.spacing(4),
     minWidth: 240,
     marginRight: theme.spacing(4),
     [theme.breakpoints.down('xs')]: {
@@ -48,6 +48,7 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: 0,
     },
   },
+  selectLabel: { fontSize: 15, top: '-4px' },
   hide: { display: 'none' },
   datePicker: {
     marginRight: 16,
@@ -74,122 +75,129 @@ const BasicInfo = () => {
   };
 
   return (
-    <Container>
-      <Grid container className={classes.root}>
-        <Grid item xs={12} sm={8}>
-          <Typography variant='h5' component='h2' className={classes.header}>
-            Tell event-attenders about your event
+    <Grid container className={classes.root}>
+      <Grid item xs={false} sm={1}></Grid>
+      <Grid item xs={12} sm={7}>
+        <Typography variant='h5' component='h2' className={classes.header}>
+          Tell event-attenders about your event
+        </Typography>
+        <Typography
+          variant='subtitle1'
+          className={clsx(classes.inputTitle, classes.marginTop)}
+        >
+          What is the name of your event?
+        </Typography>
+        <TextField
+          label='Event title'
+          InputLabelProps={{ style: { fontSize: 15 } }}
+          variant='outlined'
+          fullWidth
+        />
+        <FormControl
+          variant='outlined'
+          className={clsx(classes.formControl, classes.marginTop)}
+        >
+          <InputLabel className={classes.selectLabel}>Category</InputLabel>
+          <Select
+            style={{ height: 48 }}
+            value={category}
+            onChange={handleChange}
+            label='Category'
+          >
+            <MenuItem value='tech'>Tech</MenuItem>
+            <MenuItem value='sports'>Sports</MenuItem>
+            <MenuItem value='games'>Games</MenuItem>
+            <MenuItem value='anime'>Anime</MenuItem>
+            <MenuItem value='relationships'>Relationships</MenuItem>
+            <MenuItem value='foodAnddrinks'>Food and drinks</MenuItem>
+            <MenuItem value='performance'>Performance and visual arts</MenuItem>
+            <MenuItem value='government'>Government & politics</MenuItem>
+            <MenuItem value='schoole'>School activities</MenuItem>
+            <MenuItem value='scienceAndTechnology'>
+              Science and technology
+            </MenuItem>
+            <MenuItem value='seasons'>Seasons and holidays</MenuItem>
+            <MenuItem value='fashion'>Fashion & beauty</MenuItem>
+            <MenuItem value='movies'>Movies and entertainment</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl
+          variant='outlined'
+          className={clsx(classes.formControl, classes.marginTop)}
+        >
+          <InputLabel className={classes.selectLabel}>Platform</InputLabel>
+          <Select
+            style={{ height: 48 }}
+            value={platform}
+            onChange={(event) => setPlatform(event.target.value)}
+            label='Platform'
+          >
+            <MenuItem value='googleMeet'>Google Meet</MenuItem>
+            <MenuItem value='teams'>Microsoft teams</MenuItem>
+            <MenuItem value='youtube'>Youtube</MenuItem>
+            <MenuItem value='zoom'>Zoom</MenuItem>
+          </Select>
+        </FormControl>
+        <div style={{ display: 'flex' }} className={classes.marginTop}>
+          <TextField
+            label='Add tags'
+            variant='outlined'
+            fullWidth
+            onFocus={() => setIsTagFocus(true)}
+            onBlur={() => setIsTagFocus(false)}
+          />
+          <Button variant='outlined' className={classes.btn}>
+            Add
+          </Button>
+        </div>
+        <Typography
+          variant='body2'
+          className={`${!isTagFocus && classes.hide}`}
+        >
+          Improve discoverability of your event by adding tags relevant to the
+          subject matter.
+        </Typography>
+
+        {/* Date and time */}
+        <>
+          <Typography
+            variant='subtitle1'
+            component='h3'
+            className={classes.marginTop}
+          >
+            Date and time
           </Typography>
           <Typography
             variant='subtitle1'
-            className={clsx(classes.inputTitle, classes.marginTop)}
+            component='p'
+            style={{ marginTop: 4 }}
           >
-            What is the name of your event?
+            Let interested candidate know when the event starts.
           </Typography>
-          <TextField
-            label='Event title'
-            variant='outlined'
-            className={classes.textField}
-          />
-          <FormControl variant='outlined' className={classes.formControl}>
-            <InputLabel id='demo-simple-select-outlined-label'>
-              Category
-            </InputLabel>
-            <Select value={category} onChange={handleChange} label='Category'>
-              <MenuItem value='tech'>Tech</MenuItem>
-              <MenuItem value='sports'>Sports</MenuItem>
-              <MenuItem value='games'>Games</MenuItem>
-              <MenuItem value='anime'>Anime</MenuItem>
-              <MenuItem value='relationships'>Relationships</MenuItem>
-              <MenuItem value='foodAnddrinks'>Food and drinks</MenuItem>
-              <MenuItem value='performance'>
-                Performance and visual arts
-              </MenuItem>
-              <MenuItem value='government'>Government & politics</MenuItem>
-              <MenuItem value='schoole'>School activities</MenuItem>
-              <MenuItem value='scienceAndTechnology'>
-                Science and technology
-              </MenuItem>
-              <MenuItem value='seasons'>Seasons and holidays</MenuItem>
-              <MenuItem value='fashion'>Fashion & beauty</MenuItem>
-              <MenuItem value='movies'>Movies and entertainment</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl variant='outlined' className={classes.formControl}>
-            <InputLabel id='demo-simple-select-outlined-label'>
-              Platform
-            </InputLabel>
-            <Select
-              value={platform}
-              onChange={(event) => setPlatform(event.target.value)}
-              label='Platform'
-            >
-              <MenuItem value='googleMeet'>Google Meet</MenuItem>
-              <MenuItem value='teams'>Microsoft teams</MenuItem>
-              <MenuItem value='youtube'>Youtube</MenuItem>
-              <MenuItem value='zoom'>Zoom</MenuItem>
-            </Select>
-          </FormControl>
-          <div style={{ display: 'flex' }} className={classes.marginTop}>
+          <form noValidate>
             <TextField
-              label='Add tags'
+              label='Start Date and time'
+              type='datetime-local'
               variant='outlined'
-              className={classes.textField}
-              onFocus={() => setIsTagFocus(true)}
-              onBlur={() => setIsTagFocus(false)}
+              className={clsx(classes.datePicker, classes.marginTop)}
+              InputLabelProps={{
+                shrink: true,
+              }}
             />
-            <Button variant='outlined' className={classes.btn}>
-              Add
-            </Button>
-          </div>
-          <Typography
-            variant='body2'
-            className={`${!isTagFocus && classes.hide}`}
-          >
-            Improve discoverability of your event by adding tags relevant to the
-            subject matter.
-          </Typography>
-
-          {/* Date and time */}
-          <>
-            <Typography
-              variant='Subtitle1'
-              component='h3'
-              className={classes.marginTop}
-            >
-              Date and time
-            </Typography>
-            <Typography
-              variant='Subtitle1'
-              component='p'
-              style={{ marginTop: 4 }}
-            >
-              Let interested candidate know when the event starts.
-            </Typography>
-            <form noValidate className={classes.marginTop}>
-              <TextField
-                label='Start Date and time'
-                type='datetime-local'
-                variant='outlined'
-                className={classes.datePicker}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-              <TextField
-                label='End Date and time'
-                type='datetime-local'
-                variant='outlined'
-                className={classes.datePicker}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </form>
-          </>
-        </Grid>
+            <TextField
+              label='End Date and time'
+              type='datetime-local'
+              variant='outlined'
+              className={clsx(classes.datePicker, classes.marginTop)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </form>
+        </>
       </Grid>
-    </Container>
+      <Grid item xs={false} sm={4}></Grid>
+    </Grid>
   );
 };
 
