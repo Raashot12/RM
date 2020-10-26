18231824781afe3aa9a5c72f2863d2c13b257083
Iskilu Rasheed Akanni <66404246+Raashot12@users.noreply.github.com>
2020-10-26 05:24:02 +0100

    Updated

--- src/Components/pages/Signup&Login/Login.js
+++ src/Components/pages/Signup&Login/Login.js
@@ -31,82 +31,84 @@ const  Login = ( )=>{
 
 		return (
       <>
-        <div className="signupwrapper">
-          <Navbar />
-          <section className="signup-parent">
-            <div className="loginForm">
-              <img src={logo} className="App-logo" alt="logo" />
-              <div className="welcome">
-                <h3>Welcome Back!</h3>
-              </div>
-              <form onSubmit={handleSubmit} className="form_parent">
-                <span>
-                  <input
-                    type="email"
-                    name="email"
-                    value={login.email}
-                    size="50"
-                    required
-                    onChange={handleChange}
-                  />
-                  <p>Email address</p>
-                  <label>
-                    <i class="fas fa-envelope"></i>
-                  </label>
-                </span>
-                <span>
-                  <input
-                    type="password"
-                    name="password"
-                    value={login.password}
-                    size="50"
-                    required
-                    onChange={handleChange}
-                  />
-                  <p>Password</p>
-                  <label>
-                    <i className="fas fa-lock"></i>
-                  </label>
-                </span>
+        <div>
+          <div className="signupwrapper">
+            <Navbar />
+            <section className="signup-parent">
+              <div className="loginForm">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div className="welcome">
+                  <h3>Welcome Back!</h3>
+                </div>
+                <form onSubmit={handleSubmit} className="form_parent">
+                  <span>
+                    <input
+                      type="email"
+                      name="email"
+                      value={login.email}
+                      size="50"
+                      required
+                      onChange={handleChange}
+                    />
+                    <p>Email address</p>
+                    <label>
+                      <i class="fas fa-envelope"></i>
+                    </label>
+                  </span>
+                  <span>
+                    <input
+                      type="password"
+                      name="password"
+                      value={login.password}
+                      size="50"
+                      required
+                      onChange={handleChange}
+                    />
+                    <p>Password</p>
+                    <label>
+                      <i className="fas fa-lock"></i>
+                    </label>
+                  </span>
 
-                <div>
-                  <button
-                    type="submit"
-                    className="btn10"
-                    style={{ marginTop: "15px" }}
-                  >
-                    Log in
+                  <div>
+                    <button
+                      type="submit"
+                      className="btn10"
+                      style={{ marginTop: "15px" }}
+                    >
+                      Log in
+                    </button>
+                  </div>
+
+                  <div className="withtheflowing">or with the following:</div>
+                </form>
+                <div className="gmail_facebook">
+                  <button className="btn4">
+                    <FaFacebook />
+                    &nbsp;Facebook
+                  </button>
+                  <button className="btn1">
+                    <SiGmail />
+                    &nbsp;Gmail
                   </button>
                 </div>
 
-                <div className="withtheflowing">or with the following:</div>
-              </form>
-              <div className="gmail_facebook">
-                <button className="btn4">
-                  <FaFacebook />
-                  &nbsp;Facebook
-                </button>
-                <button className="btn1">
-                  <SiGmail />
-                  &nbsp;Gmail
-                </button>
+                <p className="terms">
+                  By clicking{" "}
+                  <a href="#/" className="getstarted">
+                    "Log in"
+                  </a>{" "}
+                  or "Continue with Facebook", I accept the RM Terms Of Service,
+                  Community Guidelines and have read the Privacy Policy.
+                </p>
+                <Link to="/ResetPassword">
+                  <p className="terms getstarted"> Forget password ?</p>
+                </Link>
               </div>
-
-              <p className="terms">
-                By clicking{" "}
-                <a href="#/" className="getstarted">
-                  "Log in"
-                </a>{" "}
-                or "Continue with Facebook", I accept the RM Terms Of Service,
-                Community Guidelines and have read the Privacy Policy.
-              </p>
-              <Link to="/ResetPassword">
-                <p className="terms getstarted"> Forget password ?</p>
-              </Link>
-            </div>
-          </section>
+            </section>
+          </div>
+          <Footer />
         </div>
-        <Footer />
       </>
     );
 	}
--- src/Components/pages/Signup&Login/Signup.js
+++ src/Components/pages/Signup&Login/Signup.js
@@ -39,83 +39,174 @@ import validates from "./validatesignup"
 
 
 		return (
-			< >
+      <>
+        <main style={{height: "100%"}}>
+          <div className="signupwrapper">
+            <Navbar />
+            <section className="signup-parent">
+              <div className="loginForm">
+                <img src={logo} className="App-logo" alt="logo" />
+                <form onSubmit={handleSubmit} className="form_parent">
+                  <span>
+                    <input
+                      type="text"
+                      name="firstName"
+                      value={values.firstName}
+                      onChange={handleChange}
+                      size="50"
+                    />
+                    <p>First Name</p>
+                    <label>
+                      <i class="fas fa-user"></i>
+                    </label>
+                  </span>
+                  {errors.firstName && (
+                    <p className="errors"> {errors.firstName}</p>
+                  )}
+                  <span>
+                    <input
+                      type="text"
+                      name="lastName"
+                      value={values.lastName}
+                      onChange={handleChange}
+                      size="50"
+                    />
+                    <p>Last Name</p>
+                    <label>
+                      <i className="fas fa-user"></i>
+                    </label>
+                  </span>
+                  {errors.lastName && (
+                    <p className="errors"> {errors.lastName}</p>
+                  )}
+                  <span>
+                    <input
+                      type="email"
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      size="50"
+                    />
+                    <p>Email address</p>
+                    <label>
+                      <i class="fas fa-envelope"></i>
+                    </label>
+                  </span>
+                  {errors.email && <p className="errors"> {errors.email}</p>}
+                  <span>
+                    <input
+                      type="tel"
+                      name="phone"
+                      value={values.phone}
+                      onChange={handleChange}
+                      size="50"
+                    />
+                    <p>Phone Number</p>
+                    <label>
+                      <i className="fas fa-phone-volume"></i>
+                    </label>
+                  </span>
+                  {errors.phone && <p className="errors"> {errors.phone}</p>}
+                  <span>
+                    <input
+                      type="text"
+                      name="username"
+                      value={values.username}
+                      onChange={handleChange}
+                      size="50"
+                    />
+                    <p>Username</p>
+                    <label>
+                      <i className="fas fa-user"></i>
+                    </label>
+                  </span>
+                  {errors.username && (
+                    <p className="errors"> {errors.username}</p>
+                  )}
+                  <span>
+                    <input
+                      type="password"
+                      name="password"
+                      value={values.password}
+                      onChange={handleChange}
+                      size="50"
+                    />
+                    <p>Password</p>
+                    <label>
+                      <i className="fas fa-lock"></i>
+                    </label>
+                  </span>
+                  {errors.password && (
+                    <p className="errors"> {errors.password}</p>
+                  )}
+                  <span>
+                    <input
+                      type="password"
+                      name="confirmPassword"
+                      value={values.confirmPassword}
+                      onChange={handleChange}
+                      size="50"
+                    />
+                    <p>Confirm Password</p>
+                    <label>
+                      <i className="fas fa-lock"></i>
+                    </label>
+                  </span>
+                  {errors.confirmPassword && (
+                    <p className="errors"> {errors.confirmPassword}</p>
+                  )}
 
-				<div className="signupwrapper">
-				<Navbar />
-					<section className="signup-parent">
-						<div className="loginForm">
-							<img src={logo} className="App-logo" alt="logo" />
-							<form    onSubmit={handleSubmit} className="form_parent">
-							<span>
-								<input type="text" name="firstName" value={values.firstName} onChange={handleChange}  size="50"  />
-								<p>First Name</p>
-								<label ><i class="fas fa-user"></i></label>
-							</span>
-								{errors.firstName && <p className="errors"> {errors.firstName}</p>}
-							<span>
-								<input type="text" name="lastName" value={values.lastName} onChange={handleChange}  size="50"/>
-								<p>Last Name</p>
-								<label ><i className="fas fa-user"></i></label>
-							</span>
-								{errors.lastName && <p className="errors"> {errors.lastName}</p>}
-								<span>
-								<input type="email" name="email" value={values.email} onChange={handleChange}  size="50"  />
-									<p>Email address</p>
-									<label ><i class="fas fa-envelope"></i></label>
-								</span>
-								{errors.email && <p className="errors"> {errors.email}</p>}
-							<span>
-								<input type="tel" name="phone" value={values.phone} onChange={handleChange}  size="50"  />
-								<p>Phone Number</p>
-								<label ><i className="fas fa-phone-volume"></i></label>
-							</span>
-								{errors.phone && <p className="errors"> {errors.phone}</p>}
-							<span>
-								<input type="text" name="username" value={values.username} onChange={handleChange} size="50" />
-								<p>Username</p>
-								<label ><i className="fas fa-user"></i></label>
-							</span>
-								{errors.username && <p className="errors"> {errors.username}</p>}
-								<span>
-								<input type="password" name="password" value={values.password} onChange={handleChange}  size="50"  />
-									<p>Password</p>
-									<label ><i className="fas fa-lock"></i></label>
-								</span>
-								{errors.password && <p className="errors"> {errors.password}</p>}
-								<span>
-									<input type="password" name="confirmPassword" value={values.confirmPassword} onChange={handleChange}  size="50" />
-									<p>Confirm Password</p>
-									<label ><i className="fas fa-lock"></i></label>
-								</span>
-								{errors.confirmPassword && <p className="errors"> {errors.confirmPassword}</p>}
-							
-								<label>
-								<input type="checkbox" name="checked" value={checked} onChange={handleChange}  style={{ marginBottom: "2px" }} /> Remember me  
-							</label>
-								<div>
-									<button type="submit" className="btn10" disabled={doSomething()}>Sign up</button>
-								</div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="checked"
+                      value={checked}
+                      onChange={handleChange}
+                      style={{ marginBottom: "2px" }}
+                    />{" "}
+                    Remember me
+                  </label>
+                  <div>
+                    <button
+                      type="submit"
+                      className="btn10"
+                      disabled={doSomething()}
+                    >
+                      Sign up
+                    </button>
+                  </div>
 
-								<div className='withtheflowing' >or with the following:</div>
-							</form>
-							<div className="gmail_facebook">
-								<button className='btn4' ><FaFacebook />&nbsp;Facebook</button>
-								<button className='btn1' ><SiGmail />&nbsp;Gmail</button>
-							</div>
+                  <div className="withtheflowing">or with the following:</div>
+                </form>
+                <div className="gmail_facebook">
+                  <button className="btn4">
+                    <FaFacebook />
+                    &nbsp;Facebook
+                  </button>
+                  <button className="btn1">
+                    <SiGmail />
+                    &nbsp;Gmail
+                  </button>
+                </div>
 
-							<p className="terms">
-								By clicking <a href='#/' className='getstarted'>"Sign up"</a> or "Continue with Facebook",
-								I accept the RM Terms Of Service, Community
-								Guidelines and have read the Privacy Policy.
-							</p>
-							<Link to="/ResetPassword"><p className="terms getstarted" > Forget password ?</p></Link>
-						</div>
-						<Footer />
-					</section>
-					</div>
-			</>
-		)
+                <p className="terms">
+                  By clicking{" "}
+                  <a href="#/" className="getstarted">
+                    "Sign up"
+                  </a>{" "}
+                  or "Continue with Facebook", I accept the RM Terms Of Service,
+                  Community Guidelines and have read the Privacy Policy.
+                </p>
+                <Link to="/ResetPassword">
+                  <p className="terms getstarted"> Forget password ?</p>
+                </Link>
+              </div>
+              <Footer />
+            </section>
+          </div>
+        </main>
+      </>
+    );
 } 
 export default Signup;
 
